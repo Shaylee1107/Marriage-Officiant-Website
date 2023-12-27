983ae1df96dbfbfb6e6574cc2a88985c1b786e1e
Shaylee <shayleebaumgartner1107@gmail.com>
2023-12-27 18:20:45 -0500

    designing services

--- app.js
+++ app.js
@@ -13,20 +13,5 @@ setTimeout(function(){
         e.target.parentElement.remove(); 
     })
 
-//faq
-// let acc = document.getElementsByClassName("accordian");
-// for(let i = 0; i < acc.length; i++){
-//     acc[i].addEventListener("click", function(){
-//         this.classList.toggle("active");
-//         this.parentElement.classList.toggle("active");
 
-//         let pannel = this.nextElementSibling;
-
-//         if(pannel.style.display === "block"){
-//             pannel.style.display = "none";
-//         } else {
-//             pannel.style.display = "block";
-//         }
-//     })
-// }
 
