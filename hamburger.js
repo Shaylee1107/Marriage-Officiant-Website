1ffbb7907eb8691d41cf097b7027cb13e88da05d
Shaylee <shayleebaumgartner1107@gmail.com>
2024-01-27 21:02:01 -0500

    adding DOMs to hamburger JS

--- hamburger.js
+++ hamburger.js
@@ -1,6 +1,20 @@
 const hamburger = document.getElementById('hamburger');
+const header = document.querySelectorAll('header');
+const designSection = document.getElementById('section-design');
+const lisaSection = document.getElementById('meet-lisa');
+const homeContactSection = document.getElementById('contact-section');
+const rawpixelSection = document.getElementById('rawpixel-section');
+const designImg = document.getElementById('main-img');
+const designText = document.getElementById('design-text-container');
+
 
 hamburger.addEventListener('click', function(){
     console.log('clicked');
-    
+    designSection.classList.add('hide');
+    lisaSection.classList.add('hide');
+    homeContactSection.classList.add('hide');
+    rawpixelSection.classList.add('hide');
+    designImg.classList.add('hide');
+    designText.classList.add('hide');
+ 
 });
