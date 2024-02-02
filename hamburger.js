69194b4d3def6680b6bfcabcbe0a5087b5de75d4
Shaylee <shayleebaumgartner1107@gmail.com>
2024-02-01 19:02:23 -0500

    adding a div for the hambruger menu

--- hamburger.js
+++ hamburger.js
@@ -1,20 +1,21 @@
 const hamburger = document.getElementById('hamburger');
-const header = document.querySelectorAll('header');
+const header = document.querySelector('header');
 const designSection = document.getElementById('section-design');
 const lisaSection = document.getElementById('meet-lisa');
 const homeContactSection = document.getElementById('contact-section');
 const rawpixelSection = document.getElementById('rawpixel-section');
 const designImg = document.getElementById('main-img');
 const designText = document.getElementById('design-text-container');
+const body = document.querySelector('body');
+const htmlBurgerDiv = document.getElementsByClassName('hamburger-div');
 
 
 hamburger.addEventListener('click', function(){
     console.log('clicked');
-    designSection.classList.add('hide');
-    lisaSection.classList.add('hide');
-    homeContactSection.classList.add('hide');
-    rawpixelSection.classList.add('hide');
-    designImg.classList.add('hide');
-    designText.classList.add('hide');
- 
+    const myDiv = document.createElement("div");
+    myDiv.classList.add('myBurger');
+    header.append(myDiv);
+    htmlBurgerDiv.classList.remove('hide');
+    myDiv.append(htmlBurgerDiv);
+
 });
